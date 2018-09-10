b3d45b1dd651e71f001633741646918a90fc806b
Albert Steppi <albert_steppi@hms.harvard.edu>
2018-09-10 16:00:14 -0400

    updated script for finding groundings and getting complexes from ungrounded ppi extractions

--- nlm_ppi/src/get_complexes.py
+++ nlm_ppi/src/get_complexes.py
@@ -7,7 +7,7 @@ from indra.tools import assemble_corpus as ac
 from functools import lru_cache
 
 
-@lru_cache(10000)
+@lru_cache(1000)
 def get_groundings(name):
     """Return a dictionary of groundings for a term.
     uses the trips processor.
@@ -38,28 +38,26 @@ def get_pmid(pmcid):
     return id_lookup(pmcid, idtype='pmcid')['pmid']
 
 
-class HGNC(object):
-    def __init__(self):
-        hgnc = pd.read_csv('~/indra/indra/resources/hgnc_entries.tsv',
-                           sep='\t')
-        hgnc['new_index'] = hgnc['HGNC ID'].apply(lambda x:
-                                                  x.split(':')[1])
-        hgnc = hgnc.set_index('new_index')
-        self.__hgnc = hgnc
-    
-    def get_symbol(self, hgnc_id):
-        return self.__hgnc.loc[hgnc_id]['Approved Symbol']
+_hgnc = pd.read_csv('~/indra/indra/resources/hgnc_entries.tsv',
+                    sep='\t')
+_hgnc['new_index'] = _hgnc['HGNC ID'].apply(lambda x:
+                                            x.split(':')[1])
+_hgnc = _hgnc.set_index('new_index')
+
+
+@lru_cache(1000)
+def get_symbol(hgnc_id):
+    return _hgnc.loc[hgnc_id]['Approved Symbol']
 
 
 def get_agent(text):
-    hgnc_converter = HGNC()
     db_refs = get_groundings(text)
     be = db_refs.get('FPLX')
     hgnc = db_refs.get('HGNC')
     if be:
         name = be
     elif hgnc:
-        name = hgnc_converter.get_symbol(hgnc)
+        name = get_symbol(hgnc)
     else:
         name = re.sub(r"\s+", '_', text)
     return Agent(name=name, db_refs=db_refs)
@@ -74,12 +72,13 @@ def get_complex(row):
     return Complex(members=[agent1, agent2], evidence=evidence)
 
 
-df = pd.read_excel("../input/pmcids_interactions1_full_predict1.xlsx",
-                   dtype='str')
-df['Statements'] = df.apply(get_complex, axis=1)
+if __name__ == '__main__':
+    df = pd.read_excel("../input/pmcids_interactions1_full_predict1.xlsx",
+                       dtype='str')
+    df['Statements'] = df.apply(get_complex, axis=1)
 
-true_stmts = list(df[df['ppi'] == 'True']['Statements'].values)
-false_stmts = list(df[df['ppi'] == 'False']['Statements'].values)
+    true_stmts = list(df[df['ppi'] == 'True']['Statements'].values)
+    false_stmts = list(df[df['ppi'] == 'False']['Statements'].values)
 
-ac.dump_statements(true_stmts, '../work/nlm_ppi_true_statements.pkl')
-ac.dump_statements(false_stmts, '../work/nlm_ppi_false_statements.pkl')
+    ac.dump_statements(true_stmts, '../work/nlm_ppi_true_statements.pkl')
+    ac.dump_statements(false_stmts, '../work/nlm_ppi_false_statements.pkl')
