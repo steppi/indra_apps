8ebd57e3bd21f2b1e2b5fd05bf22eada6fbdee23
Albert Steppi <albert_steppi@hms.harvard.edu>
2018-09-26 17:22:39 -0400

    changed fuzzy matching of sentences to incorporate partial ratios. added some doc strings

--- nlm_ppi/src/normalize_sentences.py
+++ nlm_ppi/src/normalize_sentences.py
@@ -5,36 +5,90 @@ from copy import deepcopy
 import pickle
 from fuzzywuzzy import fuzz
 from itertools import combinations
-import regex
-import random
+from indra.statements import Complex, Evidence
 
 
 def filter_none(stmts):
+    """Filter statements including an empty agent.
+
+    Creates a copy. Does not work in place
+
+    Parameters
+    __________
+    stmts : list of Indra Statements
+
+    Returns
+    -------
+    list of Indra Statements : subset of input statements that contain two or
+    more agents.
+    """
     filtered = []
     for stmt in stmts:
-        if all(stmt.agent_list()) and len(stmt.agent_list()) == 2:
+        if all(stmt.agent_list()) and len(stmt.agent_list()) > 1:
+            filtered.append(deepcopy(stmt))
+    return filtered
+
+
+def split_complexes(stmts):
+    """Split complexe statements  with more than two members into multiple
+    complex statements.
+
+    Parameters
+    ----------
+    stmts : list of Indra Statements
+
+    Returns
+    list of Indra Statements : a potentially larger list of indra statements.
+    each complex statement with more than two members will be replaced with
+    multiple complex statements: one statement for each pair of agents in the
+    original complex.
+    """
+    filtered = []
+    for stmt in stmts:
+        if len(stmt.agent_list()) > 2 and type(stmt) == Complex:
+            text = stmt.evidence[0].text
+            pmid = stmt.evidence[0].pmid
+            source_api = stmt.evidence[0].source_api
+            source_id = stmt.evidence[0].source_id
+            evidence = Evidence(source_api=source_api, source_id=source_id,
+                                pmid=pmid, text=text)
+            for agent1, agent2 in combinations(stmt.agent_list(), 2):
+                new_stmt = Complex(members=[agent1, agent2],
+                                   evidence=evidence)
+                filtered.append(new_stmt)
+        else:
             filtered.append(deepcopy(stmt))
     return filtered
 
 
 def sentence_match(sen1, sen2, cutoff1=0.8, cutoff2=0.5):
-    ratio = fuzz.ratio(sen1, sen2)
+    """Determine if sentences preprocessed by different readers are actually
+    the same.
+
+    Parameters
+    ----------
+    sen1: string
+    sen2: string
+    Two strings containing sentences
+
+    cutoff1: float
+    two sentences match if the levenstein ratio between them is greater than
+    cutoff1
+
+    cutoff2: float
+    if the levenstein ratio is less than cutoff1 but greater than cutoff2, the
+    sentences match if the partial_ratio between sentence1 and sentence2 is
+    greater than cutoff1
+
+    Returns
+    -------
+    bool: True if the sentences match
+    """
+    ratio = fuzz.ratio(sen1, sen2)/100
     if ratio > cutoff1:
         return True
     elif ratio > cutoff2:
-        if ' xref ' in sen1 and ' xref ' not in sen2:
-            reg1 = sen1.replace(' xref ', '.+')
-            # convert between ratio cutoff and number of errors to allow
-            e = len(sen1 + sen2) - cutoff1*len(sen1 + sen2)
-            r = regex.compile(reg1 + '\{{}\}'.format(e))
-            return r.match(sen2)
-        elif ' xref ' in sen2 and ' xref ' not in sen1:
-            sen1, sen2 = sen2, sen1
-            reg1 = sen1.replace(' xref ', '.+')
-            # convert between ratio cutoff and number of errors to allow
-            e = len(sen1 + sen2) - cutoff1*len(sen1 + sen2)
-            r = regex.compile(reg1 + '\{{}\}'.format(e))
-            return r.match(sen2)
+        return fuzz.partial_ratio(sen1, sen2)
     else:
         return False
 
@@ -64,18 +118,21 @@ def get_pmid_mapping(stmts):
 def deduplicate_mapping(mapping_df, cutoff1=0.8, cutoff2=0.5):
     new_mapping_df = deepcopy(mapping_df)
     new_mapping_df['normed_sentence'] = new_mapping_df['sentence']
-    groups = mapping_df.groupby(level=['pmid'])
+    groups = new_mapping_df.groupby(level=['pmid'])
     for _, new_df in groups:
         G = nx.Graph()
         for index, extraction in new_df.iterrows():
-            G.add_node(index, sentence=extraction['sentence'])
+            G.add_node(index, sentence=extraction['normed_sentence'])
         node_pairs = combinations(G.nodes(data=True), 2)
         for (index1, data1), (index2, data2) in node_pairs:
-            sentence1, sentence2 = data1['sentence'], data2['sentence']
-            if sentence_match(sentence1, sentence2):
+            sentence1, sentence2 = (data1['sentence'],
+                                    data2['sentence'])
+            if sentence_match(sentence1, sentence2,
+                              cutoff1=cutoff1, cutoff2=cutoff2):
                 G.add_edge(index1, index2)
         for component in nx.connected_components(G):
-            least_sentence = min(G.node[x]['sentence'] for x in component)
+            least_sentence = min(G.node[x]['sentence']
+                                 for x in component)
             indices = list(component)
             new_mapping_df.loc[indices, 'normed_sentence'] = least_sentence
     new_mapping_df['sentence_id'] = \
