e47e806775c1bab1b78a0bfab0d86b24712e0815
Ubuntu <ubuntu@ip-172-31-34-152.ec2.internal>
2018-09-28 13:59:53 +0000

    refactored sentence tokenizer training

--- nlm_ppi/src/train_sent_tokenizer.py
+++ nlm_ppi/src/train_sent_tokenizer.py
@@ -3,16 +3,39 @@ import pickle
 from nltk.tokenize import PunktSentenceTokenizer
 
 
-training_corpus = []
-for subdir, dirs, files in os.walk('../input/pmc_articles/'):
-    for filename in files:
-        filepath = subdir + os.sep + filename
-        if filepath.endswith('.txt'):
-            with open(filepath, 'r') as f:
-                training_corpus.append(f.read())
+def get_articles(path):
+    output = []
+    for subdir, dirs, files in os.walk(path):
+        for filename in files:
+            filepath = subdir + os.sep + filename
+            if filepath.endswith('.txt'):
+                with open(filepath, 'r') as f:
+                    output.append(f.read())
+    return output
 
-training_corpus = " ".join(training_corpus)
-tokenizer = PunktSentenceTokenizer()
-tokenizer.train(training_corpus)
-with open('bio_sent_tokenizer.pkl', 'wb') as f:
-    pickle.dump(tokenizer, f)
+
+_root_path = '../input/pmc_articles/'
+_journals = ['BMC_Bioinformatics',
+             'BMC_Biochem',
+             'BMC_Cancer',
+             'BMC_Cell_Biol',
+             'BMC_Biol',
+             'BMC_Mol_Biol',
+             'Biochemistry',
+             'Bioinformatics',
+             'Biol_Cell',
+             'Cancer',
+             'Cell']
+             
+
+if __name__ == '__main__':
+    training_corpus = []
+    for journal in _journals:
+        training_corpus.extend(get_articles(_root_path +
+                                            journal))
+
+    training_corpus = " ".join(training_corpus)
+    tokenizer = PunktSentenceTokenizer()
+    tokenizer.train(training_corpus)
+    with open('bio_sent_tokenizer.pkl', 'wb') as f:
+        pickle.dump(tokenizer, f)
